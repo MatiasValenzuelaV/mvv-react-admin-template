a3e2b77db45d8f1beeda357557a6b21f365eda28
MatiasValenzuelaV <dev.matiasvalenzuela@gmail.com>
2021-09-23 22:57:55 -0300

    Inicialización de projecto: Template admin react redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+document.title = 'dev Matías Valenzuela - Admin Template';
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
